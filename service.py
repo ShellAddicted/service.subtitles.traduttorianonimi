bd3357b8be286ce0214880a1ae111bbc2f6e36b4
ShellAddicted <shelladdicted@gmail.com>
2016-05-22 20:44:43 +0200

    Version 2.0.2

--- service.py
+++ service.py
@@ -19,166 +19,135 @@
 #  MA 02110-1301, USA.
 #  
 #
-import os;
-import sys;
-import unicodedata;
-import xbmc;
-import xbmcvfs;
-import xbmcaddon;
-import xbmcgui;
-import xbmcplugin;
 
-import urllib;
-import requests;
-import zipfile;
-import StringIO;
-import traceback;
+import xbmc
+import xbmcvfs
+import xbmcaddon
+import xbmcgui
+import xbmcplugin
 
-addon = xbmcaddon.Addon();
-author = addon.getAddonInfo("author");
-scriptid = addon.getAddonInfo("id");
-scriptname = addon.getAddonInfo("name");
-version = addon.getAddonInfo("version");
-language = addon.getLocalizedString;
-main_url = "http://traduttorianonimi.it";
+import os
+import sys
+import unicodedata
+import urllib
+import requests
+import zipfile
+import StringIO
+import shutil
 
-cwd = xbmc.translatePath(addon.getAddonInfo("path")).decode("utf-8");
-profile = xbmc.translatePath(addon.getAddonInfo("profile")).decode("utf-8");
-resource = xbmc.translatePath(os.path.join(cwd, "resources", "lib")).decode("utf-8");
-temp = xbmc.translatePath(os.path.join(profile, "temp","")).decode("utf-8");
-        
+addon = xbmcaddon.Addon()
+cwd = xbmc.translatePath(addon.getAddonInfo("path")).decode("utf-8")
+resource = xbmc.translatePath(os.path.join(cwd, "resources", "lib")).decode("utf-8")
+
 sys.path.append(resource)
+from utils import *
+log=log.getChild("Service")
 
 import TraduttoriAnonimi
 
-def log(msg):
-    xbmc.log(("""*** [{}] -> {}""".format(scriptname,msg)).encode("utf-8"), level = xbmc.LOGNOTICE)
-    
-def notify(msg,header=scriptname,time=5000,image=None):
-    if (image!=None):
-        xbmc.executebuiltin((u"Notification({},{},{},{})".format(header, msg,time,image)).encode("utf-8"))
-    else:
-        xbmc.executebuiltin((u"Notification({},{},{})".format(header, msg, time)).encode("utf-8"))
-        
-def RetriveURL(url):
-    try:
-        headers={"user-agent": "Kodi-SubtitleService-TraduttoriAnonimi"};
-        q=requests.get(url,headers=headers);
-        return q;
-    except:
-        log(traceback.format_exc())
-        notify("Errore di rete. controlla la tua connessione",time=3000)
-        return None;
-    
-def normalizeString(String2Normalize):
-    return unicodedata.normalize("NFKD",unicode(unicode(String2Normalize,"utf-8").encode("ascii","ignore")))
+def CleanJunk():
+    #cleanup junk files
+    for x in [profile,temp]:
+        if (xbmcvfs.exists(x)):
+            shutil.rmtree(x)
+            
+        xbmcvfs.mkdirs(x)
 
 def GetParams():
     param={}
     if (len(sys.argv[2])>=2):
         for pairsofparam in sys.argv[2].replace("?","").split("&"):
-            tmp=pairsofparam.split("=");
+            tmp=pairsofparam.split("=")
             if (len(tmp)==2):
-                param[tmp[0]]=tmp[1];
-    return param;
+                param[tmp[0]]=tmp[1]
+    return param
 
 def search(item):
-    try:
-        if ('ita' in item['languages']):
-            if (item['tvshow']):
-                x=TraduttoriAnonimi.TraduttoriAnonimi();
-                results=x.GrabSubtitle(item["tvshow"], int(item["season"]), int(item["episode"]));
-                if (results!=None):
-                    for result in results:
-                        subs=download(result["URL"])
-                        for sub in subs:
-                            listitem = xbmcgui.ListItem(label='Italian',label2=os.path.basename(sub),thumbnailImage='it') #sub["Name"]
-                            xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]), url = "plugin://{}/?action=download&url={}".format(scriptid,sub), listitem = listitem, isFolder = False)#sub["URL"]
-                        xbmcplugin.endOfDirectory(int(sys.argv[1]))	# send end of directory to XBMC
+    CleanJunk()
+    if ('ita' in item['languages']):
+        if (item['tvshow']):
+            x=TraduttoriAnonimi.TraduttoriAnonimi()
+            results=x.GrabSubtitle(item["tvshow"], int(item["season"]), int(item["episode"]))
+            if (results!=None):
+                for result in results:
+                    subs=download(result["URL"])
+                    for sub in subs:
+                        listitem = xbmcgui.ListItem(label='Italian',label2=os.path.basename(sub),thumbnailImage='it') #sub["Name"]
+                        xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]), url = "plugin://{}/?action=download&url={}".format(scriptid,sub), listitem = listitem, isFolder = False)#sub["URL"]
+                    xbmcplugin.endOfDirectory(int(sys.argv[1]))	# send end of directory to XBMC
             else:
-                notify("i sottotitoli sono disponibili solo per le serie tv il supporto ad i Film sara' aggiunto in futuro",time=12000)
-                log('TraduttoriAnonimi only works with tv shows. Skipped')
+                log.info("NO RESULTS")
         else:
-            notify('i sottotitoli sono disponibili solo in italiano controlla le tue impostazioni [Video->Sottotitoli->Lingue per cui scaricare i sottotitoli]',time=17000)
-            log('TraduttoriAnonimi only works with italian. Skipped')
-    except requests.ConnectionError:
-        log(traceback.format_exc())
-        notify("Errore di rete. controlla la tua connessione",time=3000)
+            notify(language(30002),time=12000) #The Subtitles are available only for TV Shows,MOVIES will be supported in future
+            log.info('TraduttoriAnonimi only works with tv shows. Skipped')
+    else:
+        notify(language(30003),time=17000) #The Subtitles are available only in Italian,Check Your Kodi Settings [Video->Subtitles->Languages to download subtitles for]
+        log.info('TraduttoriAnonimi only works with italian. Skipped')
         
 def download(url):
-    log("Downloading => {}".format(url))
+    log.debug("Downloading => {}".format(url))
     if (os.path.isfile(url)):
-        log("url is a local path")
+        log.info("url is a local path")
         return [url]
     exts=["srt"]
     out=[]
     
     r=RetriveURL(url)
     if (r!=None):
-        content=r.content;
-        tmp=StringIO.StringIO(content)
+        content=r.content
+        tmp=StringIO.StringIO(content) # "with" can't be used because StringIO has not __exit__
         if (content[0]=="P"):
-            log("ZipFile Detected")
-            q=zipfile.ZipFile(tmp);
-            for name in q.namelist():
-                if (name.split(".")[-1] in exts):
-                    q.extract(name, temp)
-                    out.append(os.path.join(temp,name))
-            q.close();
+            log.info("ZipFile Detected")
+            with zipfile.ZipFile(tmp) as q:
+                for name in q.namelist():
+                    if (name.split(".")[-1] in exts):
+                        q.extract(name, temp)
+                        out.append(os.path.join(temp,name))
         else:
-            log("Unpacked file detected")
-            try:
-                if (os.path.basename(url).split(".")[-1] in exts):
-                    q=open(os.path.join(temp,os.path.basename(url)),"wb")
+            log.info("Unpacked file detected")
+            if (os.path.basename(url).split(".")[-1] in exts):
+                with open(os.path.join(temp,os.path.basename(url)),"wb") as q:
                     q.write(content)
-                    q.close()
-                    out.append(os.path.join(temp,os.path.basename(url)))
-                else:
-                    log("Downloaded File ({}) is not in exts[{}]".format(os.path.basename(url),str(exts)))
-            except Exception as exc:
-                try:q.close();
-                except:pass
-                raise exc;
-    return out;
+                out.append(os.path.join(temp,os.path.basename(url)))
+            else:
+                log.info("Downloaded File ({}) is not in exts[{}]".format(os.path.basename(url),str(exts)))
+    return out
 
 def main():
-    log("Application version: {}".format(version))
+    log.info("Application version: {}".format(version))
     if (xbmc.Player().isPlayingVideo()):
-        for x in [profile,temp]:
-            if (not xbmcvfs.exists(x)):
-                xbmcvfs.mkdirs(x)
-    params = GetParams()
+        params = GetParams()
     
-    if (params["action"] == "search" or params["action"] == "manualsearch"):
-        item = {}
-        item["mansearch"] = params["action"] == "manualsearch"
+        if (params["action"] == "search" or params["action"] == "manualsearch"):
+            item = {}
+            item["mansearch"] = params["action"] == "manualsearch"
+            
+            if (item['mansearch']):
+                notify(language(30004),time=7000) #The Manual Search is not supported yet, but it will be in the future.
+            
+            item["season"] = str(xbmc.getInfoLabel("VideoPlayer.Season"))                    # Season
+            item["episode"] = str(xbmc.getInfoLabel("VideoPlayer.Episode")).zfill(2)					# Episode
+            item["tvshow"] = MagicUnicode(xbmc.getInfoLabel("VideoPlayer.TVshowtitle"))			# Show
+            item["file_original_path"] = xbmc.Player().getPlayingFile().decode("utf-8")           	# Full path
+            item["languages"] = []
+            
+            if ("searchstring" in params):
+                item["mansearch"] = True
+                item["mansearchstr"] = urllib.unquote(params["searchstring"])
+            
+            for lang in urllib.unquote(params["languages"]).decode("utf-8").split(","):
+                item["languages"].append(xbmc.convertLanguage(lang, xbmc.ISO_639_2))
+            
+            if (item["episode"].lower().find("s") > -1): # Check if season is "Special"
+                item["season"] = "0"
+                item["episode"] = item["episode"][-1:]
+            search(item)
         
-        if (item['mansearch']):
-            notify("La Ricerca Manuale Non e' supportata ma verra' aggiunta in futuro",time=3000)
-        
-        item["season"] = str(xbmc.getInfoLabel("VideoPlayer.Season"));                              # Season
-        item["episode"] = str(xbmc.getInfoLabel("VideoPlayer.Episode")).zfill(2);					# Episode
-        item["tvshow"] = normalizeString(xbmc.getInfoLabel("VideoPlayer.TVshowtitle"));				# Show
-        item["file_original_path"] = xbmc.Player().getPlayingFile().decode("utf-8");             	# Full path
-        item["languages"] = [];
-        
-        if ("searchstring" in params):
-            item["mansearch"] = True
-            item["mansearchstr"] = urllib.unquote(params["searchstring"])
-        
-        for lang in urllib.unquote(params["languages"]).decode("utf-8").split(","):
-            item["languages"].append(xbmc.convertLanguage(lang, xbmc.ISO_639_2))
-        
-        if (item["episode"].lower().find("s") > -1): # Check if season is "Special"
-            item["season"] = "0"
-            item["episode"] = item["episode"][-1:]
-        search(item)
-    
-    elif (params["action"] == "download"):
-        subs=download(params["url"])
-        for sub in subs:
-            listitem = xbmcgui.ListItem(label = sub)
-            xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]), url = sub, listitem = listitem, isFolder = False)
-        xbmcplugin.endOfDirectory(int(sys.argv[1]))
+        elif (params["action"] == "download"):
+            subs=download(params["url"])
+            for sub in subs:
+                listitem = xbmcgui.ListItem(label = sub)
+                xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]), url = sub, listitem = listitem, isFolder = False)
+            xbmcplugin.endOfDirectory(int(sys.argv[1]))
         
 main()
